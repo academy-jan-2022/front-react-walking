be131793726ed72a0d7b33b6034649942c40e014
Brian Hoang <brian.hd.hoang@codurance.com>
2022-03-07 17:06:58 +0000

    real green

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,26 +5,9 @@ const axios = require('axios');
 
 jest.mock("axios");
 
-test('renders header', () => {
-    render(<App/>);
-    const heading = screen.getByRole('heading');
+let matcher = 'https://gateway.marvel.com/v1/public/characters?apikey=cb0bf27ee604b7033dac0e8988a429ea';
 
-    expect(heading).toBeInTheDocument();
-    expect(heading).toHaveTextContent('Superheroes');
-});
-
-
-test('superheroes api is called', async () => {
-    axios.get.mockImplementation(() => Promise.resolve());
-    let matcher = 'https://gateway.marvel.com/v1/public/characters?apikey=cb0bf27ee604b7033dac0e8988a429ea';
-    await act(async () => {
-        await render(<App/>);
-    });
-
-    expect(axios.get).toHaveBeenCalledWith(matcher);
-});
-
-test('display list of super hero names', async () => {
+beforeEach(async () => {
     const list = {
         data:
             {data:
@@ -35,11 +18,25 @@ test('display list of super hero names', async () => {
     };
 
     axios.get.mockImplementation(() => Promise.resolve(list));
-    let matcher = 'https://gateway.marvel.com/v1/public/characters?apikey=cb0bf27ee604b7033dac0e8988a429ea';
     await act(async () => {
         await render(<App/>);
     });
+})
 
+
+test('renders header', () => {
+    const heading = screen.getByRole('heading');
+
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent('Superheroes');
+});
+
+
+test('superheroes api is called', async () => {
+    expect(axios.get).toHaveBeenCalledWith(matcher);
+});
+
+test('display list of super hero names', async () => {
     const heroesList = screen.getByRole('list');
 
     expect(heroesList).toHaveTextContent('Bat man');
