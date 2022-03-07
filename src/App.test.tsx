95967648463142b4facc09cf75d9949e219c6d62
danihra-codurance <daniel.rodriguez@codurance.com>
2022-03-07 16:59:28 +0100

    red

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
+import axios from "axios";
 
- test('renders header', () => {
-   render(<App />);
-   const heading= screen.getByRole('heading');
 
-   expect(heading).toBeInTheDocument();
-   expect(heading).toHaveTextContent('Superheroes');
- });
+jest.mock("axios");
+
+test('renders header', () => {
+    render(<App/>);
+    const heading = screen.getByRole('heading');
+
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent('Superheroes');
+});
+
+
+test('superheroes api is called', () => {
+    let matcher = 'https://gateway.marvel.com/v1/public/characters?apikey=cb0bf27ee604b7033dac0e8988a429ea';
+
+    render(<App/>);
+
+    expect(axios.get).toHaveBeenCalledWith(matcher);
+});
 
