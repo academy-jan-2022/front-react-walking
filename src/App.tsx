e5e1f4fb6a189394b72614a1f57e3dbd806bfa3f
Brian Hoang <39828877+bhoang93@users.noreply.github.com>
2022-03-10 16:34:26 +0000

    green (#7)
    * green
    
    * really green
    
    Co-authored-by: Brian Hoang <brian.hd.hoang@codurance.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import {getCachedResult, setCachedResult} from "./Services/HeroCache";
+import HttpClient from "./Services/HttpClient";
 
 export type Hero = {
     name: string
@@ -15,15 +16,56 @@ export type Hero = {
 
 function App() {
     const [heroes, setHeroes] = useState<Array<Hero>>([]);
+    const [page, setPage] = useState(1);
     const navigate = useNavigate();
 
     useEffect(() => {
-        axios.get('https://gateway.marvel.com/v1/public/characters?series=24229&apikey=cb0bf27ee604b7033dac0e8988a429ea')
-            .then(({data}) => setHeroes(data.data.results));
+        const cachedResult = getCachedResult(0);
+
+        if (cachedResult) {
+            setHeroes(cachedResult);
+        } else {
+            HttpClient.get({url: 'https://gateway.marvel.com/v1/public/characters', queryParams: {
+                    apikey: 'cb0bf27ee604b7033dac0e8988a429ea',
+                    limit: '10'
+                }})
+                .then((data: any) => {
+                    setHeroes(data.data.results)
+                    setCachedResult("0", data.data.results);
+                })
+        }
     }, []);
 
+    const fetchNextPage = () => {
+        const cachedResult = getCachedResult(page);
+        if (cachedResult) {
+            setHeroes(cachedResult);
+        } else {
+            HttpClient.get({url: 'https://gateway.marvel.com/v1/public/characters', queryParams: {
+                    apikey: 'cb0bf27ee604b7033dac0e8988a429ea',
+                    offset: (page * 10).toString(),
+                    limit: '10'
+                }})
+                .then((data: any) => {
+                    setHeroes(data.data.results)
+                    setCachedResult(page.toString(), data.data.results);
+                })
+        }
+        setPage(page + 1);
+        window.scrollTo(0, 0);
+    }
+
+    const fetchPrevPage = () => {
+        const cachedResult = getCachedResult(page - 2);
+        if (cachedResult) {
+            setHeroes(cachedResult);
+        }
+        setPage(page - 1);
+        window.scrollTo(0, 0);
+    }
+
     return (
-        <>
+        <div>
             <header>
                 <h1 aria-label='title'>Avengers</h1>
             </header>
@@ -42,13 +84,17 @@ function App() {
                             </div>
                             <div>
                                 <h2>{hero.name}</h2>
-                                <button onClick={() => moveToHeroPage(hero.id)}>more info</button>
+                                <button aria-label='link-button' onClick={() => moveToHeroPage(hero.id)}>more info</button>
                             </div>
                         </li>
                     })
                 }
             </ul>
-        </>
+            <div>
+                {page > 1 && <button onClick={fetchPrevPage}>Prev</button>}
+                <button onClick={fetchNextPage}>Next</button>
+            </div>
+        </div>
     );
 }
 
