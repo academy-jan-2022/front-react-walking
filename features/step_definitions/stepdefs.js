32d08bb517927d9f0830913bbd1380cba31617a8
danihra-codurance <daniel.rodriguez@codurance.com>
2022-03-14 11:26:36 +0100

    working gherkin

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,3 +1,24 @@
 const assert = require('assert');
-const { Given, When, Then } = require('@cucumber/cucumber');
+const {Given, When, Then} = require('@cucumber/cucumber');
 
+Given('I am on the homepage', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+
+Given('Heroes are shown with event', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+
+When('I click the event button', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('I am directed to an events page', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
