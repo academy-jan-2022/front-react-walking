6ca2aa574eb89231b171bed2719e87c0118d5052
Simon <simonbesteiro@gmail.com>
2022-03-14 12:56:58 +0100

    green

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,7 +1,9 @@
 const { Given, When, Then, AfterAll } = require('@cucumber/cucumber');
-const { Builder, By, Capabilities, Key } = require('selenium-webdriver');
+const { Builder, By, Capabilities, Key, WebElement} = require('selenium-webdriver');
 
 require("chromedriver");
+const assert = require('assert')
+const {screen} = require("@testing-library/react");
 
 // driver setup
 const capabilities = Capabilities.chrome();
@@ -11,8 +13,8 @@ const driver = new Builder().withCapabilities(capabilities).build();
 
 Given('I am on the homepage', async function () {
     await driver.get('http://localhost:3000');
-
-    return 'on homepage'
+    let titleText = await driver.findElement(By.tagName("h1")).getText();
+    assert.equal(titleText, "Avengers");
 });
 
 
